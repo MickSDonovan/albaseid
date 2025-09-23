7a4612d7ab6188656ee1fe08cb9bc5c90fde7235
MickSDonovan <michel.colacino@gmail.com>
2025-09-23 21:34:49 +0100

    :gear: build: first commit : Init Node, Express, Prisma

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import "dotenv/config";
+import express from "express";
+import router from "./routes/index.routes.js";
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.use(express.json());
+
+app.use(router);
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+});
---
+++ src/routes/index.routes.js
@@ -0,0 +1,7 @@
+import { Router } from "express";
+import health from "./health.routes.js";
+const router = Router();
+
+router.use("/health", health);
+
+export default router;
