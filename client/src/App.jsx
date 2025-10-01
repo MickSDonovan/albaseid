9da6e38e02ddd9adad42e99c642fd11f8963778d
MickSDonovan <michel.colacino@gmail.com>
2025-10-01 20:06:29 +0100

    🌟 feat(app): Downliad Image and stock in the useState

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,51 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [avatar, setAvatar] = useState(
+    "https://img.daisyui.com/images/profile/demo/superperson@192.webp"
+  );
+
+  // Fonction appelée quand l'utilisateur choisit une image
+  const handleImageUpload = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      const imageUrl = URL.createObjectURL(file);
+      setAvatar(imageUrl);
+    }
+  };
+
   return (
-    <>
-      <h1>ALBASEID</h1>
-    </>
+    <div className="flex flex-col items-center min-h-screen bg-gray-100">
+      <header className="bg-blue-600 w-full p-4 text-white text-center">
+        <h1>ALBASEID</h1>
+      </header>
+
+      <div className="flex-grow flex flex-col items-center pt-5 gap-2">
+        <div className="avatar">
+          <div className="w-56 rounded">
+            <img src={avatar} alt="Avatar" />
+          </div>
+        </div>
+
+        {/* Input caché pour sélectionner un fichier */}
+        <input
+          type="file"
+          accept="image/*"
+          onChange={handleImageUpload}
+          id="uploadInput"
+          className="hidden"
+        />
+
+        {/* Bouton pour déclencher l'input */}
+        <label htmlFor="uploadInput" className="btn btn-primary cursor-pointer">
+          Changer Avatar
+        </label>
+        <p>
+          Le but du jeu ici est de télécharger une nouvelle image pour l'avatar.
+        </p>
+      </div>
+    </div>
   );
 }
 
